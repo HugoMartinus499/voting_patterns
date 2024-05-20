1234ea160575d06ef53a053ac5168a34f2f80458
Hugo von Essen-Müller <hugo.von.essen@gmail.com>
2024-05-20 14:52:25 +0200

    Filewriting script expand

--- CSV_writing_script.py
+++ CSV_writing_script.py
@@ -8,23 +8,67 @@ Created on Sun May 19 20:23:48 2024
 titles = ["RANDOM STATE", "GLOBALS", "TURTLES", "PATCHES", "LINKS", "PLOTS", "EXTENSIONS"]
 
 
-file = open("C:/Users/hugov/Github/voting_patterns/Data/ABM netlogo world.txt", 'r')
+file = open("C:/Users/hugov/Github/voting_patterns/Data/TXT_world_files/Sim_1_16_80.txt", 'r')
 
 
-to_file = open('Data/dummy.txt', 'w')
+to_file = open('C:/Users/hugov/Github/voting_patterns/Data/dummy.txt', 'w')
 for line in file :
     new_file = False
     for t in titles:
         if t in line:
             filename = line
-            to_file = open(f'Data/CSV/{t}.csv', 'w')
+            to_file = open(f'C:/Users/hugov/Github/voting_patterns/Data/CSV/{t}_1_16_80.csv', 'w')
             new_file = True
     
     if not new_file:
         to_file.write(line)
         
     
+file = open("C:/Users/hugov/Github/voting_patterns/Data/TXT_world_files/Sim_1_16_100.txt", 'r')
+
+
+to_file = open('C:/Users/hugov/Github/voting_patterns/Data/dummy.txt', 'w')
+for line in file :
+    new_file = False
+    for t in titles:
+        if t in line:
+            filename = line
+            to_file = open(f'C:/Users/hugov/Github/voting_patterns/Data/CSV/{t}_1_16_100.txt.csv', 'w')
+            new_file = True
+    
+    if not new_file:
+        to_file.write(line)        
         
+file = open("C:/Users/hugov/Github/voting_patterns/Data/TXT_world_files/Sim_1_18_80.txt", 'r')
+
+
+to_file = open('C:/Users/hugov/Github/voting_patterns/Data/dummy.txt', 'w')
+for line in file :
+    new_file = False
+    for t in titles:
+        if t in line:
+            filename = line
+            to_file = open(f'C:/Users/hugov/Github/voting_patterns/Data/CSV/{t}_1_18_80.csv', 'w')
+            new_file = True
+    
+    if not new_file:
+        to_file.write(line)
+        
+    
+file = open("C:/Users/hugov/Github/voting_patterns/Data/TXT_world_files/Sim_1_18_100.txt", 'r')
+
+
+to_file = open('C:/Users/hugov/Github/voting_patterns/Data/dummy.txt', 'w')
+for line in file :
+    new_file = False
+    for t in titles:
+        if t in line:
+            filename = line
+            to_file = open(f'C:/Users/hugov/Github/voting_patterns/Data/CSV/{t}_1_18_100.txt.csv', 'w')
+            new_file = True
+    
+    if not new_file:
+        to_file.write(line)   
         
     
     
